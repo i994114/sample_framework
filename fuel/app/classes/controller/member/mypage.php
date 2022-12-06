9821153211d26dc503425efb72ba0b97e562f3db
i994114 <i994114@hotmail.com>
2022-12-06 16:48:04 +0900

    宿題：ログアウト機能を実装

--- fuel/app/classes/controller/member/mypage.php
+++ fuel/app/classes/controller/member/mypage.php
@@ -4,12 +4,42 @@ class Controller_Member_Mypage extends Controller
 {
     public function action_index()
     {
+        $error = '';
+        $formData = '';
+
+        //認証用のインスタンス生成
+        $auth = \Auth\Auth::instance();
+
+        //ログアウト用のボタン作成
+        $form = \Fuel\Core\Fieldset::forge('logoutform');
+        $form->add('submit', '', array('type' => 'submit', 'value' => 'ログアウト'));
+
+        //ログインしているユーザがいるかをチェック
+        if (!Auth::check()) {
+            $auth = 0;
+            //不正ログインによりログイン画面にリダイレクト
+            \Fuel\Core\Response::redirect('/login');
+
+        } else {
+            if (\Fuel\Core\Input::method() === 'POST') {
+                //ログアウトメッセージを表示
+                \Fuel\Core\Session::set_flash('sucMsg', 'ログアウトしました');
+
+                //ログアウトしてログイン画面に戻る
+                $auth->logout();
+                \Fuel\Core\Response::redirect('login');
+            }
+            $form->repopulate();
+        }
+
+
         //変数としてビューを割り当てる
         $view = View::forge('template/index');
         $view->set('head',View::forge('template/head'));
         $view->set('header',View::forge('template/header'));
         $view->set('contents',View::forge('member/mypage'));
         $view->set('footer',View::forge('template/footer'));
+        $view->set_global('logoutform', $form->build(''), false);
 
         // レンダリングした HTML をリクエストに返す
         return $view;
--- fuel/app/views/member/mypage.php
+++ fuel/app/views/member/mypage.php
@@ -1,4 +1,8 @@
 <div style="text-align: center;">
     <h1>マイページです。</h1>
+    <div class="logout-area" style="text-align: center">
+        <?=$logoutform?>
+    </div>
+
 </div>
 
